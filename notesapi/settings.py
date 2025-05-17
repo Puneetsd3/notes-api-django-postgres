6c1a8dc53c4c82e78352574d5939100748b77511
Puneetsd3 <170717126+Puneetsd3@users.noreply.github.com>
2025-05-17 14:59:44 +0530

    Fix DATABASES not defined error

--- notesapi/settings.py
+++ notesapi/settings.py
@@ -78,7 +78,13 @@ WSGI_APPLICATION = 'notesapi.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgres://postgres:postgres@localhost:5432/postgres',  # placeholder fallback
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
